cb4470898b2387d3c4e24a2ca28cb75194711e96
lleejong <lleejong@gmail.com>
2016-12-18 22:14:11 +0900

    run.sh 수정

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 args=("$@");
-java -cp ./target/NS3Client-0.0.1-SNAPSHOT.jar com.choilab.proj.skt.App ${args[0]} ${args[1]} ${args[2]} ${args[3]} ${args[4]} ${args[5]} ${args[6]}
+java -cp ./target/NS3Client-0.0.1-SNAPSHOT.jar com.choilab.proj.skt.App ${args[0]} ${args[1]} ${args[2]} ${args[3]} ${args[4]} ${args[5]} ${args[6]} ${args[7]}
